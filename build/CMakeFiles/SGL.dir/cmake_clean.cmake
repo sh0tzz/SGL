ece3837d261c8c46b80f83c32dd80f8dc7aad257
sh0tzz <lubranier@gmail.com>
2022-06-14 10:24:01 +0200

    gitignore test

---
+++ build/CMakeFiles/SGL.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SGL.dir/SGL/SGL_Funcs.cpp.o"
+  "CMakeFiles/SGL.dir/SGL/SGL_Label/SGL_Label.cpp.o"
+  "CMakeFiles/SGL.dir/SGL/SGL_Parent/SGL_Parent.cpp.o"
+  "CMakeFiles/SGL.dir/SGL/SGL_Widget/SGL_Widget.cpp.o"
+  "CMakeFiles/SGL.dir/main.cpp.o"
+  "SGL"
+  "SGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
